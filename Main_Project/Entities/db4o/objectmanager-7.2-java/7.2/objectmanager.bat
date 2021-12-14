433a2530d87f7f628d6f4164bbaaed29562400fd
Nasrul Wahabi <wahabinasrul@gmail.com>
2021-12-14 10:55:38 +0700

    Delete Main_Project directory

--- Main_Project/Entities/db4o/objectmanager-7.2-java/7.2/objectmanager.bat
+++
@@ -1,1 +0,0 @@
-java -jar objectmanager.jar
--- Main_Project/Repository/DatatInteRepo.java
+++
@@ -1,9 +0,0 @@
-package com.javaoop.tugas.SMT_3.Project_Akhir.Main_Project.Repository;
-
-public interface DatatInteRepo {
-    
-    public void setDataToDatabase(String nama, String username,String password,String aktor, String index);
-
-    public abstract void setDataToDatabase(int tingkat, String jurusan);
-
-}
